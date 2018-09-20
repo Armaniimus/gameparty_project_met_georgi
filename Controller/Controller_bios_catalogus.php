3e2fb364cb82b0bce4c1658fc75fa558041fca1a
Armand_laptop <517226@edu.rocmn.nl>
2018-09-20 08:51:46 +0200

    Simple read create update delete route

---
+++ Controller/Controller_bios_catalogus.php
@@ -0,0 +1,55 @@
+<?php
+    /**
+     *
+     */
+    class Controller_bios_catalogus
+    {
+        function __construct($method, $params = FALSE) {
+            $this->method = $method;
+            if ($params != FALSE) {
+                $this->params = $params;
+            }
+        }
+
+        public function runController() {
+            switch ($this->method) {
+                case 'create':
+                    return $this->create();
+                    break;
+
+                case 'update':
+                    return $this->update();
+                    break;
+
+                case 'delete':
+                    if (isset($this->params[0]) ) {
+                        $id = $this->params[0];
+                        return $this->delete($id);
+                    } else {
+                        return $this->read();
+                    }
+                    break;
+
+                default:
+                    return $this->read();
+                    break;
+            }
+        }
+
+        public function create() {
+            return "The method create is called";
+        }
+
+        public function read() {
+            return "The method read is called";
+        }
+
+        public function update() {
+            return "The method update is called";
+        }
+
+        public function delete() {
+            return "The method delete is called";
+        }
+    }
+?>
