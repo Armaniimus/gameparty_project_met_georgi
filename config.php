7bf322c53ddceaa978b112a3347914979aec9905
Armaniimus_home <Tommy_chester@hotmail.com>
2018-10-04 20:17:11 +0200

    created basis for the css wong pathfile bugfix

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
 Define("BESTAND_DIEPTE", 3);
 
+$url = $_SERVER['REQUEST_URI'];
+$packets = explode("/", $url);
+$path = array_slice($packets, 0, BESTAND_DIEPTE);
+$path = implode($path, "/");
+Define("APP_DIR", $path);
+
 Define("DB_NAME", "Gameplayparties");
 Define("DB_USERNAME", "root");
 Define("DB_PASS", "");
