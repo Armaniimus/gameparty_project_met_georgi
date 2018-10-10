5a23d6fdb98ba51e3d0ff4a81235252bc36ec073
leanderVanDerOest <leander1999@me.com>
2018-10-10 13:07:04 +0200

    changed href for css styling sheets

--- View/header.php
+++ View/header.php
@@ -2,8 +2,8 @@
   <head>
     <meta charset="utf-8">
     <title>GameplayParties</title>
-    <link rel="stylesheet" href="View/css/master.css">
-    <link rel="stylesheet" href="View/css/grid-v1.3.1.css">
+    <link rel="stylesheet" href="../View/css/master.css">
+    <link rel="stylesheet" href="../View/css/grid-v1.3.1.css">
   </head>
   <body>
 
