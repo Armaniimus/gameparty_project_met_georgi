49627a1fd92aad99bb7f35d595b9a73241db1748
leanderVanDerOest <leander1999@me.com>
2018-10-10 13:06:25 +0200

    added layout for catalogus

---
+++ View/catalogus.php
@@ -0,0 +1,25 @@
+<?php require_once('header.php') ?>
+<body>
+	<div class="row cinema-container">
+
+		<?php foreach ($sample as $array_key => $value) {
+			?>
+				<div class="cinema">
+					<div class="image-container">
+
+						<img src="../View/images/960x280_kinepolisbreda_9.jpg" alt="">
+
+					</div>
+					
+					<div class="information-container">
+						
+						 <?php echo $value['bioscoop_naam']; ?>
+
+					</div>
+
+				</div>
+
+		<?php } ?>
+			</div>
+</body>
+</html>
