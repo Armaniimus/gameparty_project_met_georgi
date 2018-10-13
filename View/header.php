18fec215a0931bc7d1f664b579bfd022ac13fa48
leanderVanDerOest <leander1999@me.com>
2018-10-13 17:47:52 +0100

    changed src directions

--- View/header.php
+++ View/header.php
@@ -2,13 +2,13 @@
   <head>
     <meta charset="utf-8">
     <title>GameplayParties</title>
-    <link rel="stylesheet" href="View/css/master.css">
-    <link rel="stylesheet" href="View/css/grid-v1.3.1.css">
     <link rel="stylesheet" href="View/css/responsive.css">
+    <link rel="stylesheet" href="../View/css/master.css">
+     <link rel="stylesheet" href="../View/css/grid-v1.3.1.css">
   </head>
   <body>
 
     <div class="content">
         <header class="siteHeader">
-          <img src="gpp.svg">
+          <img src="../View/images/gpp.svg">
         </header>
