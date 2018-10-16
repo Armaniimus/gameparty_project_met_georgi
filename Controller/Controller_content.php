b58de22a4c582ef61a87bb26b15aef0666f66e57
Georgi <gaterin98@gmail.com>
2018-10-16 04:25:35 -0700

    content wijzig pagina gefixt in nieuwe format

--- Controller/Controller_content.php
+++ Controller/Controller_content.php
@@ -41,14 +41,34 @@ class Controller_content {
 
 private function overons()
 {
+$selectQuery = "SELECT tab_titel,pagina_titel,content_naam,content,pagina_beschrijving,steekwoorden FROM content WHERE contentID='1' ";
 
-$contentEdit = $this->connection->QueryRead("SELECT content_inhoud FROM content WHERE id='overons' ");
+
+
 $main = file_get_contents("view/partials/content.html");
-$result = implode($contentEdit[0]);
+$content = $this->connection->QueryRead($selectQuery);
+
+$tab_titel = $content[0]["tab_titel"];
+$pagina_titel = $content[0]["pagina_titel"];
+$content_naam = $content[0]["content_naam"];
+$content0 = $content[0]["content"];
+$pagina_beschrijving = $content[0]["pagina_beschrijving"];
+$steekwoorden = $content[0]["steekwoorden"];
 
 $this->TemplatingSystem->setTemplateData("main-content", $main);
+
 $this->TemplatingSystem->setTemplateData("page-title", "over ons");
-$this->TemplatingSystem->setTemplateData("content-edit", $result);
+
+
+
+$this->TemplatingSystem->setTemplateData("tab_titel", $tab_titel);
+$this->TemplatingSystem->setTemplateData("pagina_titel", $pagina_titel);
+$this->TemplatingSystem->setTemplateData("content_naam", $content_naam);
+$this->TemplatingSystem->setTemplateData("content0", $content0);
+$this->TemplatingSystem->setTemplateData("pagina_beschrijving", $pagina_beschrijving);
+$this->TemplatingSystem->setTemplateData("steekwoorden", $steekwoorden);
+
+
 $this->TemplatingSystem->setTemplateData("content-id", "overons");
 $this->TemplatingSystem->setTemplateData("appdir", APP_DIR);
 $return = $this->TemplatingSystem->GetParsedTemplate();
@@ -63,16 +83,50 @@ private function sendData()
 	}else{
 		header("Location: ".APP_DIR." ");
 	}
-	if (isset($_POST["contentText"]) ) {
-		$this->contentText = $_POST["contentText"];
+	if (isset($_POST["tab_titel"])) {
+		$this->tab_titel = $_POST["tab_titel"];
+	}else{
+		header("Location: ".APP_DIR." ");
+	}
+	if (isset($_POST["pagina_titel"])) {
+		$this->pagina_titel = $_POST["pagina_titel"];
+	}else{
+		header("Location: ".APP_DIR." ");
+	}
+	if (isset($_POST["content_naam"])) {
+		$this->content_naam = $_POST["content_naam"];
+	}else{
+		header("Location: ".APP_DIR." ");
+	}
+	if (isset($_POST["content0"])) {
+		$this->content0 = $_POST["content0"];
+	}else{
+		header("Location: ".APP_DIR." ");
+	}
+	if (isset($_POST["pagina_beschrijving"])) {
+		$this->pagina_beschrijving = $_POST["pagina_beschrijving"];
+	}else{
+		header("Location: ".APP_DIR." ");
+	}
+	if (isset($_POST["steekwoorden"])) {
+		$this->steekwoorden = $_POST["steekwoorden"];
 	}else{
 		header("Location: ".APP_DIR." ");
 	}
 
-	$qry= "UPDATE `content` SET `content_inhoud`='$this->contentText' WHERE `content`.`id`='$this->contentId' ";
+
+
+
+	$qry= "UPDATE `content` SET
+	`tab_titel`='$this->tab_titel',
+	`pagina_titel`='$this->pagina_titel',
+	`content_naam`='$this->content_naam',
+	`content`='$this->content0',
+	`pagina_beschrijving`='$this->pagina_beschrijving',
+	`steekwoorden`='$this->steekwoorden' WHERE `contentID`='1' ";
 
 $contentSend = $this->connection->UpdateData($qry);
-
+var_dump($contentSend);
 header("Location: ".APP_DIR." ");
 
 }
