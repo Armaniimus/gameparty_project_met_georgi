41793f875b4929f563e11731734ede825927c286
leanderVanDerOest <leander1999@me.com>
2018-10-17 03:01:05 +0100

    created security for single read

--- Controller/Controller_bioscoop.php
+++ Controller/Controller_bioscoop.php
@@ -4,6 +4,8 @@
      */
     class Controller_bioscoop
     {
+        private $templatingSystem;
+
         function __construct($method, $params = FALSE) {
             $this->method = $method;
             if ($params != FALSE) {
@@ -12,6 +14,7 @@
 
             $this->DataHandler = new DataHandler(DB_NAME, DB_USERNAME, DB_PASS, DB_SERVER_ADRESS, DB_TYPE);
             $this->TemplatingSystem = new TemplatingSystem("View/bios_view.tpl");
+                    $this->TemplatingSystem->setTemplateData("appdir", APP_DIR);
         }
 
         public function runController() {
@@ -32,6 +35,9 @@
                         return $this->read();
                     }
                     break;
+                case 'read_single':
+                    return $this->read_single();
+                    break;
 
                 default:
                     return $this->read();
@@ -47,34 +53,92 @@
             $loginBool = $_SESSION["loginBool"];
 
             if($loginBool == 1){
-                $sql = "SELECT * FROM bioscopen";
+                $sql = "SELECT bioscoopID,bioscoop_naam,straatnaam,postcode, plaats, provincie FROM bioscopen";
                 $result = $this->DataHandler->ReadData($sql);
 
-                $return = "";
-                for ($i=0; $i < count($result); $i++) {
-                    $return .= "<div class='col-xs-12 col-s-6 col-m-4 col-m-3 float-l border'>";
-                    $row = $result[$i];
-                    foreach ($row as $key => $value) {
-                        $return .= "<div>";
-                        $return .= "$value";
-                        $return .= "</div>";
-                    }
+                   
 
-                    $return .= "</div>";
+                $grid = "";
+
+                $grid .= "<table>
+                  <thead>
+                    <tr>
+
+                      <th>bioscoopID</th>
+                      <th>bioscoop_naam</th>
+                      <th>straatnaam</th>
+                      <th>postcode</th>
+                      <th>plaats</th>
+                      <th>provincie</th>
+                      <th>Read</th>
+                      <th><a href=''>delete</a></th>      
+                    </tr>
+                  </thead><tbody>";
+
+                foreach ($result as $key => $value) {
+
+                    $grid .= "<tr>";
+
+                    foreach ($value as $key => $attributen) {
+                        $grid .= "<td>".$attributen."</td>";
+
+                
+                    }
+                    $grid .= "<td><a href='../bioscoop/read_single/".$value['bioscoopID']."'>Read</a></td>";
+                     $grid .= "</tr>";
                 }
 
-                $this->TemplatingSystem->setTemplateData("grid", $return);
+                $grid .= "</tbody>";
+
+                $grid .= "</table>";
+
+
+            
+            
+                $this->TemplatingSystem->setTemplateData("content", $grid);
                 $return = $this->TemplatingSystem->GetParsedTemplate();
 
                 return $return;
+
                 
             } else if($loginBool == 0){
-               header("Location: http://localhost/shelter/gameparty_project_met_georgi");
+               header("Location: {appdir}/gameparty_project_met_georgi");
             }
         }
 
         public function update() {
             return "The method update is called";
+        }
+
+        public function read_single(){
+             $loginBool = $_SESSION["loginBool"];
+
+            if($loginBool == 1){
+
+             $id = $this->params[0];
+
+             $sql = "SELECT * FROM bioscopen where bioscoopID = $id";
+
+             $result = $this->DataHandler->ReadData($sql);
+
+
+
+
+
+             $main = file_get_contents("view/partials/bios_read.html");
+
+             $this->TemplatingSystem->setTemplateData("content", $main);
+
+             $return = $this->TemplatingSystem->GetParsedTemplate();
+
+            } 
+
+            else if($loginBool == 0){
+               header("Location: {appdir}/gameparty_project_met_georgi");
+            }
+
+
+            return $return;
         }
 
         public function delete() {
