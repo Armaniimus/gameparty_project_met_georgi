737fc406c593249114a83240c2a858c14d505675
leanderVanDerOest <leander1999@me.com>
2018-10-18 12:41:52 +0100

    changed reservation button

--- View/catalogus.php
+++ View/catalogus.php
@@ -15,9 +15,10 @@
 					<div class="information-container">
 
 						 <?php echo $value['bioscoop_naam']; ?>
+						 <?php $bioscoop_id = $value['bioscoopID']; ?>
 						 <div class="buttons">
 						 	<div class="button-container">
-						 		<a href="">Reserveer</a>
+						 		<a href="reserveer/<?php echo $bioscoop_id; ?>">Reserveer</a>
 						 	</div>
 						 	<div class="button-container">
 						 		<a href="">Detail</a>
