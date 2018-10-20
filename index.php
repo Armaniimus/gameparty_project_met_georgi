5e02e46400a429b736696a89a2d9f2299a3d494a
Armand_laptop <517226@edu.rocmn.nl>
2018-10-20 21:25:52 +0200

    class securityHeaders gewist omdat hij niet wordt gebruikt en hij niet gebruikt gaat worden en ik er geen docblocks voor wil schrijven

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ require_once 'Model/DataValidator-v3.2.php';
 require_once 'Model/FileHandler-v1.php';
 require_once 'Model/HtmlElements-v1.1.php';
 require_once 'Model/PhpUtilities-v2.php';
-require_once 'Model/SecurityHeaders-v1.php';
 require_once 'Model/SessionModel.php';
 require_once 'Model/TemplatingSystem-v1.php';
 
