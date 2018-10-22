ae6a4adf39b3ecee3e30231460d5f63af86165b1
Armand_laptop <517226@edu.rocmn.nl>
2018-10-22 20:39:50 +0200

    docblock toegevoegt voor prepValidateBigInt in datavalidator

--- Model/DataValidator-v3.2.php
+++ Model/DataValidator-v3.2.php
@@ -520,6 +520,11 @@ class DataValidator {
         return ['min' => $min,'max'=> $max];
     }
 
+    /**
+     * this method takes an sql bigint type and returns the max and min values allowed in it
+     * @param  string $data expects 1 of 2 possible values "bigint unsigned" or "bigint"
+     * @return array        returns an assoc array with 2 values min and max
+     */
     private function PrepValidateBigInt($data) {
         if (strpos($data, 'unsigned') !== false){
             $max = (2 ** 64)-1;
